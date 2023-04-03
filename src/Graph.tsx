e416ffb1f9e13715d1e8fe9ca5f75e3b7a74fe38
Reynaldo <reynaldo.estrella@lc.cuny.edu>
2023-04-03 16:37:58 -0400

    modified componentDidUpdate

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -60,9 +60,10 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([
+          DataManipulator.generateRow(this.props.data),
+      ] as unknown as TableData
+      )
     }
   }
 }
